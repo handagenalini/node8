a67cff267fe45d6cce307fe181187f12b1ccee67
handagenalini <handagenalini@gmail.com>
2023-03-15 15:57:22 +0530

    first commit

--- util/database.js
+++ util/database.js
@@ -1,10 +1,6 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'root',
-    password: 'handage@12'
-});
+const sequelize = new Sequelize('root','root','handage@12',{dialect:'mysql',host:'localhost'}
+);
 
-module.exports = pool.promise();
+module.exports =sequelize;
